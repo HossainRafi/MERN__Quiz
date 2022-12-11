8cd8147123a28209b109b5543f2e38bffc96bda0
HossainRafi <hossainrafi71@gmail.com>
2022-12-11 16:19:20 +0600

    main structure

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useRef } from "react";
+import { Link } from "react-router-dom";
 
 const Main = () => {
-  return (
-    <div>Main</div>
-  )
-}
+  const inputRef = useRef(null);
 
-export default Main
+  return (
+    <div className="container">
+      <h1 className="title text-light">QuikQuiz</h1>
+      <ol>
+        <li>You will be asked 10 questions</li>
+        <li>Every question have ONE point</li>
+        <li>Each question have three options</li>
+        <li>You can choose only one option</li>
+        <li>You can review and change answers before finish</li>
+      </ol>
+
+      <form id="form">
+        <input ref={inputRef} type="text" placeholder="Enter Your Name" />
+      </form>
+
+      <div className="start">
+        <Link className="btn" to={"quiz"}>
+          Start Quiz
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
