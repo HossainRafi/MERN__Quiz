b1e51b39dd9bd7ff7476d0b8e82822805f47d1f2
HossainRafi <hossainrafi71@gmail.com>
2022-12-11 21:04:33 +0600

    result css

--- client/src/components/Result.js
+++ client/src/components/Result.js
@@ -11,7 +11,7 @@ export default function Result() {
 
   return (
     <div className="container">
-      <h1 className="title text-light">Quiz Application</h1>
+      <h1 className="title text-light">QuikQuiz</h1>
 
       <div className="result flex-center">
         <div className="flex">
