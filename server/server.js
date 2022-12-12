a1a75580212361e5d77c5682721fe394b8ded8a6
HossainRafi <hossainrafi71@gmail.com>
2022-12-13 00:09:47 +0600

    api end points setup

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import { config } from "dotenv";
+import router from "./router/route.js";
 
 const app = express();
 
@@ -13,6 +14,17 @@ config();
 
 /** appliation port */
 const port = process.env.PORT || 8080;
+
+/** routes */
+app.use("/api", router); /** apis */
+
+app.get("/", (req, res) => {
+  try {
+    res.json("Get Request");
+  } catch (error) {
+    res.json(error);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server connected to http://localhost:${port}`);
