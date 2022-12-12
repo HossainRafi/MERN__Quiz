61b339ae293d4e3f07916e45f12d9adb3f2a4ef4
HossainRafi <hossainrafi71@gmail.com>
2022-12-12 23:56:44 +0600

    backend basic setup

--- server/server.js
+++ server/server.js
@@ -1,16 +1,19 @@
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import { config } from "dotenv";
 
 const app = express();
 
-//********* rout ***********
-app.get("/", (req, res) => {
-  try {
-    res.json("Get Request");
-  } catch (error) {
-    res.json(error);
-  }
-});
+/** app middlewares */
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+config();
 
-app.listen(8080, () => {
-  console.log("Server is running on port http://localhost:8080");
+/** appliation port */
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`Server connected to http://localhost:${port}`);
 });
