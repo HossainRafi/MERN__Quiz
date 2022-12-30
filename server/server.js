1941e6af66da23c55aafc1dfe717770d661c760d
HossainRafi <hossainrafi71@gmail.com>
2022-12-30 20:45:14 +0600

    new commit

--- server/server.js
+++ server/server.js
@@ -4,9 +4,6 @@ import cors from "cors";
 import { config } from "dotenv";
 import router from "./router/route.js";
 
-/** import connection file */
-import connect from "./database/conn.js";
-
 const app = express();
 
 /** app middlewares */
@@ -29,17 +26,6 @@ app.get("/", (req, res) => {
   }
 });
 
-/** start server only when we have valid connection */
-connect()
-  .then(() => {
-    try {
-      app.listen(port, () => {
-        console.log(`Server connected to http://localhost:${port}`);
-      });
-    } catch (error) {
-      console.log("Cannot connect to the server");
-    }
-  })
-  .catch((error) => {
-    console.log("Invalid Database Connection");
-  });
+app.listen(port, () => {
+  console.log(`Server connected to http://localhost:${port}`);
+});
